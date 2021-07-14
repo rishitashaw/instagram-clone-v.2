d684f7abc866fae21f4b176098797b40bf027d82
Theseregrets <rishitashaw49@gmail.com>
2021-07-14 19:53:57 +0530

    data seede in firebase

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { firebase, FieldValue } from "./lib/firebase";
 import reportWebVitals from "./reportWebVitals";
+import FirebaseContext from "./context/firebase";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import Firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+//import { seedDatabase } from "../seed";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxxl1KNIOZH9W_UgRFRwfwi4VIoGTk8vE",
+  authDomain: "instagram-7e195.firebaseapp.com",
+  projectId: "instagram-7e195",
+  storageBucket: "instagram-7e195.appspot.com",
+  messagingSenderId: "712561877980",
+  appId: "1:712561877980:web:e2971fbf067bd0c2476519",
+};
+
+const firebase = Firebase.initializeApp(firebaseConfig);
+const { FieldValue } = Firebase.firestore;
+
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
